f32fbb9dcfbd8b4c3d232184f6b869c8a3a391b2
Edgar Munoz <munozedg@gmail.com>
2021-04-08 12:28:47 -0500

    ejemplo datos horribles incluido

---
+++ R/03-Leer-Archivo-CSV-coma-como-punto-decimal.R
@@ -0,0 +1,44 @@
+# Leer un archivo CSV que usa comas como punto decimal ----
+
+library(tidyverse)
+
+# Ejemplo: datos_horribles.csv
+#
+# "Fecha","Último","Vol.","% var."
+# "06.04.2021","53,92","43,54K", "-2,10%"
+# "06.04.2021","53,92","43,54K", "-2,10%"
+
+COL_ES <- locale("es", decimal_mark = ",")
+
+mi_cols <- cols(
+  `Fecha` = col_date(format = "%d.%m.%Y"),
+  `Último` = col_number(),
+  `Vol.` = col_number(),
+  `% var.` = col_number(),
+  .default = col_character()
+)
+
+test_df <- read_csv(
+  "./data/datos_horribles.csv",
+  locale = COL_ES,
+  col_types = mi_cols,
+  trim_ws = TRUE
+)
+
+test_df
+
+test_df <- test_df %>% 
+  rename(
+    fecha = Fecha,
+    ultimo = Último,
+    vol = `Vol.`,
+    pct_var = `% var.`   
+  )
+
+test_df
+
+# A tibble: 2 x 4
+# fecha      ultimo   vol pct_var
+# <date>      <dbl> <dbl>   <dbl>
+# 2021-04-06   53.9  43.5    -2.1
+# 2021-04-06   53.9  43.5    -2.1
--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # edgaR
+
 Enhanced Data Gathering for Analysis using R
 
 This is just a public colection of functions in R that I hope will be also useful to you!
 
-Una colección publica de funciones en R que espero sean de utilidad para ti también!)
+Una colección publica de funciones en R que espero sean de utilidad para ti también!
 
-# fn ABC
+# fn 1
 
-# fn XYZ
+# fn 2
